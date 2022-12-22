83086ce7413803f500d46f2910db2d9d5ba8c17e
DanBrown47 <danwand47@gmail.com>
2022-12-22 09:32:56 +0530

    Add : env example and main.py

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-    
-
 import json
 import requests
+import os
+import sys
+import argparse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+YOUTUBE_API_KEY = 
+parser =  argparse.ArgumentParser()
+
+
+
+def fetch_youtube(url, *args):
+    pass
+
+def main():
+    parser.add_argument('-y', '--youtube', help="Add the full link of Youtube video", required=True)
+
+    if not len(sys.argv) > 1:
+        parser.print_help()
+        exit()
+
+    if parser.parse_args().youtube is not None:
+        fetch_youtube(parser)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
