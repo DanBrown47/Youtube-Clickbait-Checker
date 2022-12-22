d0b213ca2ec9b3e24550d0f2ea0766ef4a9e460a
DanBrown47 <danwand47@gmail.com>
2022-12-22 11:48:10 +0530

    Complete : Thumbnail Fetching

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 import os
 import sys
 import argparse
+import re
 from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
 load_dotenv()
 
@@ -16,10 +18,37 @@ except KeyError:
 parser =  argparse.ArgumentParser()
 
 
+def is_youtube_url(url):
+    return True
+    # Compile the regular expression
+    pattern = re.compile(r'^(https?://)?(www\.)?(youtube\.com|youtu\.be)')
 
-def fetch_youtube(url, *args):
-    print(url)
-    pass
+    # Check if the string matches the pattern
+    return pattern.match(url) is not None
+
+
+def get_thumbnail(url, **kwargs):
+    api_obj = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+    request = api_obj.videos().list(
+            part="snippet",
+            id=url
+    )
+
+    response = request.execute()
+    thumbnail_url = response["items"][0]["snippet"]["thumbnails"]["high"]["url"]
+    print(thumbnail_url)
+    return thumbnail_url
+
+    # return res_url
+
+
+def fetch_youtube(url, **kwargs):
+    # Testing if the url given is a youtube link
+    if is_youtube_url(url):
+        thumbnail_url = get_thumbnail(url)
+    else:
+        print("Not a youtube url")
+
 
 def main():
     parser.add_argument('-y', '--youtube', help="Add the full link of Youtube video", required=True)
