5105363bb2d6f681845b1e5a19602148f6049206
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-27 21:33:59 +0530

    prediction routing done

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 from src.clickbait.thumbnail import Fetch
 from src.clickbait.clickbait_dect import Click
 
+#violence content prediction functions
+from src.violence.violence_detc import predict_video
 
 load_dotenv()
 
@@ -30,8 +32,26 @@ def input():
 
 @app.route('/mature')
 def mature():
-  return render_template('input.html')
+  return render_template('input-violence.html')
 
+@app.route("/predict",methods=['POST'])
+def predict():
+  YouTube_Link =  request.form['youtube'].strip()
+  # A mistake will render the link unusable
+
+  pattern = re.compile(r'^(https?://)?(www\.)?(youtube\.com|youtu\.be)')
+  if pattern.match(YouTube_Link) is not None:
+    predict = predict_video(YouTube_Link)
+    print(predict)
+    return render_template('violence-result.html', prediction=predict), 200
+  
+  else:
+    message = """
+      The link does not seems to be a Youtube link
+      It should be following a definit pattern,
+      Use the whole URL and dont use shortend links.
+    """
+    return render_template('error_5XX.html' , error=message), 500
 
 
 @app.route('/fetch', methods=['POST'])
